8614530942c5cac4969b40d7dcffcc9cf773af41
cinexin <migueguillamon@gmail.com>
2017-02-07 11:55:21 +0100

    Pig Latin Translator Exercise Finished

--- PigLatinTranslator/PigLatinTranslator.py
+++ PigLatinTranslator/PigLatinTranslator.py
@@ -1,4 +1,17 @@
 """ Pig Latin Game proposed as part of CodeAcademy Tutorial
 """
+import sys
 
-print "Pig Latin"
+print "WELCOME TO THE PIG Latin TRANSLATOR!!"
+original = raw_input ("Enter a word: ")
+
+if (len(original) == 0) or not original.isalpha():
+     print "Input string is empty or contains numeric chars"
+     sys.exit(-1)
+
+pig = "ay"
+word = original.lower()
+firstLetter = word[0]
+translatedWord = word + firstLetter + pig
+translatedWord = translatedWord[1:len(translatedWord)]
+print "Translated word: " + translatedWord
